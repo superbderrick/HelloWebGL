48f398bf229008d1ad6806f097ef50d1dcd7b150
Kang Jinyeoung <derrick@Kangs-MacBook-Pro.local>
2015-12-13 16:38:42 +0900

    changed code sturucure too corectly.

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 var express = require("express");
 var app = express();
-var router = express.Router();
 var path = require('path');
 
+var route = require("./routes/router.js");
+
+
 app.set("view engine" , 'ejs');
+app.set("views", "./views");
+
 app.use(express.static(path.join(__dirname , 'public')));
-
-app.get('/' , function (req , res) {
-	res.render('index');
-});
-
-app.get('/reset' , function (req , res) {
-	res.render('index');
-});
-
-app.get('/about' , function (req , res) {
-	res.render('about');
-});
-
-app.get('/contact' , function (req , res) {
-	res.render('about');
-});
+app.use("/", route);
 
 
 app.listen(8080,function(){
---
+++ routes/router.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var route = express.Router();
+
+//main page router
+
+route.get('/' , function (req , res) {
+	res.render('index');
+});
+
+route.get('/reset' , function (req , res) {
+	res.render('index');
+});
+
+route.get('/about' , function (req , res) {
+	res.render('about');
+});
+
+route.get('/contact' , function (req , res) {
+	res.render('about');
+});
+
+module.exports = route;
