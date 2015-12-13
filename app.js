18cb2f98533b4a1c5745faaef227c5b6279bce4f
Kang Jinyeoung <derrick@Kangs-MacBook-Pro.local>
2015-12-13 16:24:26 +0900

    code arrange

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var path = require('path');
 app.set("view engine" , 'ejs');
 app.use(express.static(path.join(__dirname , 'public')));
 
-
 app.get('/' , function (req , res) {
 	res.render('index');
 });
