2f9a4c6a8364ba7f8ff63bc143524e44884420f8
superbderrick <kang.derrick@gmail.com>
2019-03-10 16:28:48 +0900

    just a canvas example

--- routes/router.js
+++ routes/router.js
@@ -4,7 +4,7 @@ var route = express.Router();
 //main page router
 
 route.get('/' , function (req , res) {
-	res.render('position');
+	res.render('index');
 });
 route.get('/signup' , function (req , res) {
 	res.render('signup');
